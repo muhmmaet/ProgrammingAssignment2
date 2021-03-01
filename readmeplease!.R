7197b19381181c908c2644a5ca2f3fa614e57550
muhmmaet <79774910+muhmmaet@users.noreply.github.com>
2021-03-01 11:46:00 +0300

    Add files via upload

---
+++ readmeplease!.R
@@ -0,0 +1,6 @@
+number <- 5
+if(number>0) {
+print(paste(number,"positive number"))
+}
+print("others numbers...")
+## In the example, we're assigning the value 5 to the number variable. If the number value in if content is greater than 0, we add a positive number expression with the field assigned as the value to the screen, and the other numbers ... are displayed on the screen as the process continues.
